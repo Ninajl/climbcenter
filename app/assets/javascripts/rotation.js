c049b825667a41c49262c01f1af921611cd5f274
nina <ninajl@gmail.com>
2015-06-05 15:33:29 -0600

    edit rotation.js file and added image to public folder for heroku

--- app/assets/javascripts/rotation.js
+++ app/assets/javascripts/rotation.js
@@ -1,30 +1,55 @@
-var dragging = false;
-$(function() {
-    var target = $('#image');
-    var mainTarget = $('#image');
-    var rad = mainTarget.width()/2;
-    var elOfs = mainTarget.offset();
-    var elPos = {
-       x: elOfs.left,
-       y: elOfs.top
-    };
-    target.mousedown(function() {
+$(function () {
+    var dragging = false,
+        target_wp,
+        o_x, o_y, h_x, h_y, last_angle;
+    $('#image').mousedown(function (e) {
+        h_x = e.pageX;
+        h_y = e.pageY; // clicked point
+        e.preventDefault();
+        e.stopPropagation();
         dragging = true;
-    });
-    $(document).mouseup(function() {
-        dragging = false;
-    });
-    $(document).mousemove(function(e) {
-      var mPos = {
-        x: e.pageX-elPos.x,
-        y: e.pageY-elPos.y
-      };
-      var getAtan = Math.atan2(mPos.x-rad, mPos.y-rad);
-      var getDeg = -getAtan/(Math.PI/180) + 135;  //135 = (180deg-45deg)
+        target_wp = $(e.target).closest('#image');
+        if (!target_wp.data("origin")) target_wp.data("origin", {
+            left: target_wp.offset().left,
+            top: target_wp.offset().top
+        });
+        o_x = target_wp.data("origin").left;
+        o_y = target_wp.data("origin").top; // origin point
 
+        last_angle = target_wp.data("last_angle") || 0;
+    })
 
+    $(document).mousemove(function (e) {
         if (dragging) {
-            mainTarget.css({transform: 'rotate(' + getDeg + 'deg)'});
+            var s_x = e.pageX,
+                s_y = e.pageY; // start rotate point
+            if (s_x !== o_x && s_y !== o_y) { //start rotate
+                var s_rad = Math.atan2(s_y - o_y, s_x - o_x); // current to origin
+                s_rad -= Math.atan2(h_y - o_y, h_x - o_x); // handle to origin
+                s_rad += last_angle; // relative to the last one
+                var degree = (s_rad * (360 / (2 * Math.PI)));
+                target_wp.css('-moz-transform', 'rotate(' + degree + 'deg)');
+                target_wp.css('-moz-transform-origin', '50% 50%');
+                target_wp.css('-webkit-transform', 'rotate(' + degree + 'deg)');
+                target_wp.css('-webkit-transform-origin', '50% 50%');
+                target_wp.css('-o-transform', 'rotate(' + degree + 'deg)');
+                target_wp.css('-o-transform-origin', '50% 50%');
+                target_wp.css('-ms-transform', 'rotate(' + degree + 'deg)');
+                target_wp.css('-ms-transform-origin', '50% 50%');
+                console.log(degree);
+            }
         }
-    });
-});
+    }) // end mousemove
+
+    $(document).mouseup(function (e) {
+        dragging = false
+        var s_x = e.pageX,
+            s_y = e.pageY;
+
+        // Saves the last angle for future iterations
+        var s_rad = Math.atan2(s_y - o_y, s_x - o_x); // current to origin
+        s_rad -= Math.atan2(h_y - o_y, h_x - o_x); // handle to origin
+        s_rad += last_angle;
+        target_wp.data("last_angle", s_rad);
+    })
+})
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
 
   def index
+    @response = RestClient.get "https://www.kimonolabs.com/api/9doxozu2?apikey=#{ENV['API_KEY']}"
+
+    @response["results"]["Colorado_Gyms"]
+
   end
 
 end
